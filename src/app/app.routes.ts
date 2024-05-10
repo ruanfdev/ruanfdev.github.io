5d4e74fb6e4d2dbd8840418cba1c248e37960773
ruanfdev <ruanfdev@proton.me>
2024-05-10 14:55:26 +0200

    updated coming soon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ComingSoonComponent } from './coming-soon/coming-soon.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ComingSoonComponent },
+  // { path: '', component: ComingSoonComponent },
+];
