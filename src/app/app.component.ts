d313158ee2acc9413907124b6c813c035a44b031
ruanfdev <ruanfdev@proton.me>
2024-05-14 11:18:13 +0200

    updated coming soon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgOptimizedImage],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ruanfdev.github.io';
+  // <img ngSrc="assets/product-image.jpg" width="400" height="300" alt="Product" priority="high" fill="true" />
 }
