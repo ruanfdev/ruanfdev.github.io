829648a7d129971943df5387a276934ea5eddc81
ruanfdev <ruanfdev@proton.me>
2024-05-27 12:55:45 +0200

    created wedding replacement

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ComingSoonComponent } from './coming-soon/coming-soon.component';
+import { WeddingComponent } from './wedding/wedding.component';
 
 export const routes: Routes = [
-    { path: '', component: ComingSoonComponent }
+    { path: '', component: ComingSoonComponent },
+    { path: 'wedding', component: WeddingComponent }
 ];
