b5a568000a28f4ca0b15fd8e27b59f841c969a7f
ruanfdev <ruanfdev@proton.me>
2024-05-28 12:27:12 +0200

    updated general styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
-import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgOptimizedImage],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ruanfdev.github.io';
-  // <img ngSrc="assets/product-image.jpg" width="400" height="300" alt="Product" priority="high" fill="true" />
 }
--- src/app/wedding/wedding.component.ts
+++ src/app/wedding/wedding.component.ts
@@ -1,12 +1,13 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-wedding',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './wedding.component.html',
   styleUrl: './wedding.component.scss'
 })
 export class WeddingComponent {
-
+  // <img ngSrc="assets/product-image.jpg" width="400" height="300" alt="Product" priority="high" fill="true" />
 }
