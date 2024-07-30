e3308c015081f9f075706f3157559368103331fc
ruanfdev <ruanfdev@proton.me>
2024-07-30 10:18:53 +0200

    optimizations

--- src/app/wedding/wedding.component.ts
+++ src/app/wedding/wedding.component.ts
@@ -2,12 +2,12 @@ import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-wedding',
-  standalone: true,
-  imports: [NgOptimizedImage],
-  templateUrl: './wedding.component.html',
-  styleUrl: './wedding.component.scss'
+	selector: 'app-wedding',
+	standalone: true,
+	imports: [NgOptimizedImage],
+	templateUrl: './wedding.component.html',
+	styleUrl: './wedding.component.scss',
 })
 export class WeddingComponent {
-  // <img ngSrc="assets/product-image.jpg" width="400" height="300" alt="Product" priority="high" fill="true" />
+	// <img ngSrc="assets/product-image.jpg" width="400" height="300" alt="Product" priority="high" fill="true" />
 }
